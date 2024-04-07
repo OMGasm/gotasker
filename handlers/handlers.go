29ec27d6addd731a29b3b1d5c0be714921d9814e
OMGasm <OMGasm@users.noreply.github.com>
2024-04-08 00:01:23 +1000

    rewrite method handing, just learned go supports methods in the path :D

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -32,9 +32,10 @@ func RegisterHandlers(logger *slog.Logger) {
 	db = NewDB()
 
 	handlers := map[string]http.HandlerFunc{
-		"/":       defaultHandler,
-		"/delete": deleteTask,
-		"/insert": insertTask,
+		"/":            defaultHandler,
+		"POST /delete": deleteTask,
+		"POST /insert": insertTask,
+		"GET /insert":  insertTask_get,
 	}
 	for path, handler := range handlers {
 		http.HandleFunc(path, logHTTP(logger, handler))
@@ -48,46 +49,36 @@ func notFound(reason string, res http.ResponseWriter, req *http.Request) {
 	err.Render(req.Context(), res)
 }
 
+func insertTask_get(res http.ResponseWriter, req *http.Request) {
+	form := templates.Create(false)
+	form.Render(req.Context(), res)
+}
+
 func insertTask(res http.ResponseWriter, req *http.Request) {
-	switch req.Method {
-	case http.MethodGet:
-		form := templates.Create(false)
-		form.Render(req.Context(), res)
-
-	case http.MethodPost:
-		if err := req.ParseForm(); err != nil {
-			slog.Warn("Invalid form data?", "Form error", err)
-		}
-		var task Task
-		task.Title = req.PostFormValue("title")
-		task.Date = req.PostFormValue("date")
-		if task.Title == "" {
-			page := templates.Invalid("did you forget to add a title?")
-			page.Render(req.Context(), res)
-			return
-		}
-		db.AddTask(task)
-		http.Redirect(res, req, "/", http.StatusFound)
-
-	default:
-		notFound("Invalid method", res, req)
+	if err := req.ParseForm(); err != nil {
+		slog.Warn("Invalid form data?", "Form error", err)
 	}
-
+	var task Task
+	task.Title = req.PostFormValue("title")
+	task.Date = req.PostFormValue("date")
+	if task.Title == "" {
+		page := templates.Invalid("did you forget to add a title?")
+		page.Render(req.Context(), res)
+		return
+	}
+	db.AddTask(task)
+	http.Redirect(res, req, "/", http.StatusFound)
 }
 
 func deleteTask(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodDelete || req.Method == http.MethodPost {
-		id, err := strconv.ParseInt(req.PostFormValue("id"), 10, 0)
-		if err != nil {
-			slog.Warn("Invalid task id?", "Form task id", req.PostFormValue("id"))
-			res.WriteHeader(http.StatusBadRequest)
-			errPage := templates.Invalid("bad id?")
-			errPage.Render(req.Context(), res)
-			return
-		}
-		db.DeleteTask(int(id))
-		http.Redirect(res, req, "/", http.StatusFound)
-	} else {
-		notFound("Invalid method", res, req)
+	id, err := strconv.ParseInt(req.PostFormValue("id"), 10, 0)
+	if err != nil {
+		slog.Warn("Invalid task id?", "Form task id", req.PostFormValue("id"))
+		res.WriteHeader(http.StatusBadRequest)
+		errPage := templates.Invalid("bad id?")
+		errPage.Render(req.Context(), res)
+		return
 	}
+	db.DeleteTask(int(id))
+	http.Redirect(res, req, "/", http.StatusFound)
 }
